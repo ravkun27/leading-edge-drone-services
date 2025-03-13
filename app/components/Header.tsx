9493c55892c57131aaf8b3c05f09edb2d0a4d100
ravkun <sp810111@gmail.com>
2025-03-13 16:28:12 +0530

    new changes

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -19,7 +19,9 @@ export default function Header() {
   });
 
   const handleInputChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
   ) => {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
